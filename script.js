e2934447e902f1911b3edf415ee1b6033ccfad7c
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-06-27 23:02:21 +0800

    Layout draft(No email and last part sections.)

---
+++ script.js
@@ -0,0 +1,9 @@
+// function pad() {
+//   const target = document.getElementById("AboutMe");
+//   const space = document.querySelector("nav");
+
+//   console.log(space.style);
+// }
+// window.onload = () => {
+//   pad();
+// };
