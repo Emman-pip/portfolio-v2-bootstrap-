afc6fe2726be256637e47c39208bc71e3ed5ebc8
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-06-28 17:39:45 +0800

    Add scroll abserver for logo and color the email area.

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-// function pad() {
-//   const target = document.getElementById("AboutMe");
-//   const space = document.querySelector("nav");
+function scrollObserver(section, toAffect, typeOfDisplay) {
+  const option = {
+    root: null,
+    threshold: 0,
+    rootMargin: "0px",
+  };
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        console.log("inter");
+        toAffect.style.display = typeOfDisplay;
+      } else {
+        console.log("!inters");
+        toAffect.style.display = "none";
+      }
+    });
+  }, option);
+  observer.observe(section);
+}
 
-//   console.log(space.style);
-// }
-// window.onload = () => {
-//   pad();
-// };
+scrollObserver(
+  document.querySelector(".slimTop"),
+  document.querySelector(".logo"),
+  "flex"
+);
