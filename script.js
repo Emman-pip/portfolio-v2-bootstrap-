75691f33a5fc66464c2cf0a540186a44e84e0b76
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-07-01 22:47:39 +0800

    Minor bug fixes.

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ function scrollObserver(section, toAffect, typeOfDisplay) {
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
       if (!entry.isIntersecting) {
-        console.log("inter");
         toAffect.style.display = typeOfDisplay;
       } else {
-        console.log("!inters");
         toAffect.style.display = "none";
       }
     });
@@ -23,3 +21,16 @@ scrollObserver(
   document.querySelector(".logo"),
   "flex"
 );
+
+function slowScroll(element, target) {
+  element.onscroll = () => {
+    const scrolltoTop = document.scrollingElement.scrollTop;
+
+    const xvalue = "center";
+    const scrollFactor = 0.3;
+    const yValue = scrolltoTop * scrollFactor;
+    target.style.backgroundPosition = xvalue + " " + yValue + "px";
+  };
+}
+
+// slowScroll(document.querySelector("body"), document.querySelector(".section1"));
